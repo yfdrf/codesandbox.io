aa47ef49b55f4b1db3f072fdafbf5cd1a0becfbb
bsefwe <92920899+bsefwe@users.noreply.github.com>
2023-04-05 00:50:56 +0000

    Update and rename node_shell.js to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'5aaed9b7-7fe3-47c3-bb52-db59859ce198'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
